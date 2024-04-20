91f75ccdb470c7ac1cbc9c75f59c076ca7cb5ebf
Barma-lej <schick.a@gmail.com>
2024-04-20 15:25:08 +0200

    Added new eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import imp from "eslint-plugin-import";
+import globals from "globals";
+
+
+export default [
+  eslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      imp
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      }
+    },
+  }
+]
