28da59066d46d7020e44b125edf1b65c4e285337
Barma-lej <schick.a@gmail.com>
2026-04-02 13:54:48 +0200

    refactor: extract MOWER_ENTITY_DOMAINS constant for improved clarity and reuse

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
+// Card editor constants
+export const MOWER_ENTITY_DOMAINS = ['select', 'switch', 'number', 'button'];
+
+// Card constants
 // Services
 export const LAWNMOWER_SERVICE = 'lawn_mower';
 export const ACTION_MOWING = 'start_mowing';
